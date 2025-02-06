95d50c524de6cd2c9bd613d354639d5fe1b885d4
RafaelFranciss <francisacuna.821@gmail.com>
2025-02-06 13:04:28 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="name">
+      <h1>Rafael Francis T. Acuna</h1>
+      <div className="box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
